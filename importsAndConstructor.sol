649a8f11b8193ae8eb8ae06a5e85ab0adf4a8b77
Pedro Wilson <pedrowilsonsilvaff@gmail.com>
2023-12-09 10:13:11 -0300

    Update importsAndConstructor.sol

--- importsAndConstructor.sol
+++ importsAndConstructor.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FullFeatureContract is Ownable {
+    uint256 private initialValue;
+
+    constructor(uint256 _initialValue) {
+        initialValue = _initialValue;
+    }
+
+    function getInitialValue() public view onlyOwner returns (uint256) {
+        return initialValue;
+    }
+}
 
