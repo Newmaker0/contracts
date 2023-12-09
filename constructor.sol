0c7cb8738bebdf4529989cdc5c342c3a95b96e75
Pedro Wilson <pedrowilsonsilvaff@gmail.com>
2023-12-09 10:12:06 -0300

    Update constructor.sol

--- constructor.sol
+++ constructor.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ConstructorContract {
+    uint256 private initialValue;
+
+    constructor(uint256 _initialValue) {
+        initialValue = _initialValue;
+    }
+
+    function getInitialValue() public view returns (uint256) {
+        return initialValue;
+    }
+}
 
