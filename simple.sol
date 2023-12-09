5772185bde9b3261cb1c82d6546a4963ad78d18e
Pedro Wilson <pedrowilsonsilvaff@gmail.com>
2023-12-09 10:10:42 -0300

    Update simple.sol

--- simple.sol
+++ simple.sol
@@ -1,1 +1,13 @@
+pragma solidity ^0.8.0;
 
+contract SimpleContract {
+    uint256 public storedData;
+
+    function set(uint256 data) public {
+        storedData = data;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
